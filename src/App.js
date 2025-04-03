49f123e577d4e181b0bdb4f2c38f74c95004ec16
mikecurious <mikkohbrayoh@gmail.com>
2025-04-03 13:51:59 +0300

    fixed dependancies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CodeBlock from './components/CodeBlock';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yet Kenya API Documentation</h1>
+    </div>
+  );
+}
+
+export default App;
